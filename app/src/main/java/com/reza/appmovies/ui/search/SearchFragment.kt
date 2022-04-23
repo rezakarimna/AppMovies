cec6c00b6d517b6b6754d644232d149d48d2c575
Reza <karimnia.reza70@yahoo.com>
2022-04-24 01:19:13 +0430

    completing SearchFragment.kt

--- app/src/main/java/com/reza/appmovies/ui/search/SearchFragment.kt
+++ app/src/main/java/com/reza/appmovies/ui/search/SearchFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.reza.appmovies.R
 import com.reza.appmovies.databinding.FragmentSearchBinding
 import com.reza.appmovies.ui.home.LastMoviesAdapter
+import com.reza.appmovies.utils.initRecycler
+import com.reza.appmovies.utils.showInVisible
 import com.reza.appmovies.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,10 +20,10 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
     //binding
-    lateinit var binding: FragmentSearchBinding
+    private lateinit var binding: FragmentSearchBinding
 
     @Inject
-    lateinit var lastMoviesAdapter: LastMoviesAdapter
+    lateinit var searchAdapter: LastMoviesAdapter
 
     private val viewModel: SearchViewModel by viewModels()
     override fun onCreateView(
@@ -31,4 +35,60 @@ class SearchFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        searchMovies()
+        observeMoviesList()
+        setRecyclerViewMovies()
+        observeLoading()
+        observeEmptyList()
+    }
+
+    private fun searchMovies() {
+        binding.searchEdt.doAfterTextChanged {
+            val search = it.toString()
+            if (search.isNotEmpty()) {
+                callApiSearchMovies(search)
+            }
+        }
+    }
+
+    private fun callApiSearchMovies(nameMovies: String) {
+        viewModel.loadSearchMovies(nameMovies)
+    }
+
+    private fun observeMoviesList() {
+        viewModel.moviesList.observe(viewLifecycleOwner) {
+            searchAdapter.setData(it.data)
+        }
+    }
+
+    private fun setRecyclerViewMovies() {
+        binding.moviesRecycler.initRecycler(LinearLayoutManager(requireContext()), searchAdapter)
+    }
+
+    private fun observeLoading() {
+        viewModel.loading.observe(viewLifecycleOwner) {
+            if (it) {
+                binding.searchLoading.showInVisible(true)
+            } else {
+                binding.searchLoading.showInVisible(false)
+            }
+        }
+    }
+
+    private fun observeEmptyList() {
+        binding.apply {
+            viewModel.empty.observe(viewLifecycleOwner) {
+                if (it) {
+                    emptyItemsLay.showInVisible(true)
+                    moviesRecycler.showInVisible(false)
+                } else {
+                    emptyItemsLay.showInVisible(false)
+                    moviesRecycler.showInVisible(true)
+                }
+            }
+        }
+
+    }
 }
